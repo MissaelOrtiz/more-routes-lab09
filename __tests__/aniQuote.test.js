5282209b7623e6cf66f046d70807cf9a0484303f
Missael Ortiz <ortiz.missael80@gmail.com>
2021-08-03 09:50:57 -0700

    added getby id test, getbyid func

--- __tests__/aniQuote.test.js
+++ __tests__/aniQuote.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import AniQuoteService from '../lib/services/AniQuoteService.js';
 
 describe('demo routes for anime quotes', () => {
   beforeEach(() => {
@@ -13,5 +14,12 @@ describe('demo routes for anime quotes', () => {
     const someString = expect.any(String);
 
     expect(res.body).toEqual({ id: '1', quote: someString, source: someString, character: someString });
+  });
+
+  it('reads an existing quote by id via GET', async () => {
+    const quote = await AniQuoteService.generateQuote();
+    const res = await request(app).get(`/api/v1/aniQuotes/${quote.id}`);
+
+    expect(res.body).toEqual(quote);
   });
 });
