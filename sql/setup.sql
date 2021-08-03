cb524db8368beb117cadc6c0af4824479e90060f
Missael Ortiz <ortiz.missael80@gmail.com>
2021-08-03 09:22:45 -0700

    init aniquotes model, controller, service, and test

--- sql/setup.sql
+++ sql/setup.sql
@@ -5,3 +5,12 @@ CREATE TABLE quotes (
     quote TEXT NOT NULL,
     source TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS aniQuotes;
+
+CREATE TABLE aniQuotes (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    quote TEXT NOT NULL,
+    source TEXT NOT NULL,
+    'character' TEXT NOT NULL
+);
